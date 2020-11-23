2cc5a8cc068c4c6b2b1f377c1c2aa1eb4fe32402
JaviVinnas <javi.vinas.2000@gmail.com>
2020-11-24 00:49:24 +0100

    Esqueleto de la pantalla pedidos acabado

--- src/gui/PantallaPedidos.java
+++ src/gui/PantallaPedidos.java
@@ -6,17 +6,35 @@ import javax.swing.*;
 
 public class PantallaPedidos implements Pantalla{
     private JPanel mainPanel;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton volverButton;
+    private JButton confirmarButton;
+    private JButton cancelarButton;
     private JLabel pasoLabel;
     private JLabel horaLabel;
     private JLabel telefonoLabel;
-    private JTabbedPane tabbedPane1;
-    private JList list1;
-    private JButton button4;
-    private JButton button5;
-    private JTextArea textArea1;
+    private JTabbedPane panelConTablasMenuTabbedPane;
+    private JList listaItemsPedidoJList;
+    private JButton borrarButton;
+    private JButton borrarTodosButton;
+    private JTextArea infoItemPedidoTextArea;
+    private JPanel fueraDeCartaPanel;
+    private JPanel dentroDeLaCartaPanel;
+    private JList listaPrimerosJList;
+    private JList listaSegundosJList;
+    private JList listaBebidasJList;
+    private JList listaPostresJList;
+    private JTextArea InfoElementoCartaTextArea;
+    private JButton añadirAlMenúButton;
+    private JTextArea InfoMenuTextArea;
+    private JButton borrarMenúActualButton;
+    private JButton añadirMenúAlPedidoButton;
+    private JLabel postresLabel;
+    private JLabel bebidasLabel;
+    private JLabel segundosLabel;
+    private JLabel primerosLabel;
+    private JList listaFueradelMenu;
+    private JTextArea infoItemFueraDelMenu;
+    private JButton añadirAlMenúButton1;
     private final App app;
 
     public PantallaPedidos(App app){
